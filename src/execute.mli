4a5f290e93d3d94a13b4942666fb2544339a76ea
Shakeim <shakeim762@gmail.com>
2023-10-27 14:05:13 -0400

    Implemented execute/mli with specification and defined function 'execute'

--- src/execute.mli
+++ src/execute.mli
@@ -1,5 +1,7 @@
 (** Display the welcome screen *)
-val execute : unit
+
 (** Display graphics and intstruct the user how to start 
     the game when applicatino is first opened. Also presents
-    some useful controls (i.e - settings for customizable options)*)
+    some useful controls (i.e - settings for customizable 
+    options).*)
+    val execute : unit
