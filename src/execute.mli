d8aca40328cd2d55acc7372947e110e7dc9da5bf
Shakeim <shakeim762@gmail.com>
2023-10-27 14:00:45 -0400

    Implemented the mli file for execute.ml

---
+++ src/execute.mli
@@ -0,0 +1,5 @@
+(** Display the welcome screen *)
+val execute : unit
+(** Display graphics and intstruct the user how to start 
+    the game when applicatino is first opened. Also presents
+    some useful controls (i.e - settings for customizable options)*)
