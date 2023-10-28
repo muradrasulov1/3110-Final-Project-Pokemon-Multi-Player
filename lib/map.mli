e0f1fe116dbc7799f71587889c355d6fba730f68
Shakeim <shakeim762@gmail.com>
2023-10-28 19:02:00 -0400

    Added map.ml and map.mli with specification for representation type

---
+++ lib/map.mli
@@ -0,0 +1,10 @@
+open Grass
+open Tile
+
+(** The entire playing area for the user to walk on, catch Pokemon, or battle
+    other trainers on the field*)
+
+    (** Representation type for map *)
+    type t
+
+    (** *)
