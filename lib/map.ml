9956016630df941ad3799dd227790f9cb18bb77e
Murad <mr982@cornell.edu>
2023-10-28 20:00:19 -0400

    Map initialization with random-placed different-probability tile types

--- lib/map.ml
+++ lib/map.ml
@@ -1,5 +1,19 @@
-let width = 20
-let height = 20
+let width = 5
+let height = 5
+
+let init_x = width // 2
+let init_y = height // 2
+
+type command =
+  | Up
+  | Left
+  | Right
+  | Down
+
+type move =
+  | Valid
+  | Invalid
+
 
 type tile =
   | Empty
@@ -9,10 +23,32 @@ type tile =
   | FirstAid
   | Wall
 
-let initialize_map () =
-  Array.init width (fun x ->
-    Array.init height (fun y ->
-      if x = 0 || x = width - 1 || y = 0 || y = height - 1 then Wall
-      else Empty
+let prob_of = function
+| Grass -> 60
+| Path -> 20
+| Lava | FirstAid -> 10
+| _ -> 0
+
+let initialize_map_with_probabilities width height =
+  (* Initialize the Random module with a seed (can be any integer) *)
+  Random.init 42;
+  let grass_prob = prob_of Grass in
+  let path_prob = prob_of Path in
+  let lava_prob = prob_of Lava in
+
+  let map =
+    Array.init width (fun x ->
+      Array.init height (fun y ->
+        let rand_value = Random.int 100 in
+        match rand_value with
+        | _ when x = 0 || x = width - 1 || y = 0 || y = height - 1 -> Wall
+        | _ when rand_value < grass_prob -> Grass
+        | _ when rand_value < grass_prob + path_prob -> Path
+        | _ when rand_value < grass_prob + path_prob + lava_prob -> Lava
+        | _ -> FirstAid
+      )
     )
-  )
+  in
+  map
+
+
--- lib/map.mli
+++ lib/map.mli
@@ -1,10 +1,44 @@
-open Grass
-open Tile
+(** Module defining a simple game world with tiles. *)
 
-(** The entire playing area for the user to walk on, catch Pokemon, or battle
-    other trainers on the field*)
+(** The width of the game world. *)
+val width : int
 
-    (** Representation type for map *)
-    type t
+(** The height of the game world. *)
+val height : int
 
-    (** *)
+(** The initial x-coordinate of the player. *)
+val init_x : int
+
+(** The initial y-coordinate of the player. *)
+val init_y : int
+
+(** Type representing movement commands. *)
+type command =
+  | Up
+  | Left
+  | Right
+  | Down
+
+(** Type representing the result of a move. *)
+type move =
+  | Valid
+  | Invalid
+
+(** Type representing different types of tiles. *)
+type tile =
+  | Empty
+  | Grass
+  | Path
+  | Lava
+  | FirstAid
+  | Wall
+
+(** Function to calculate the probability of a tile type. *)
+val prob_of : tile -> int
+
+(** Initialize the game map with given probabilities for tile types.
+    @param width The width of the map.
+    @param height The height of the map.
+    @return An array representing the initialized map.
+*)
+val initialize_map_with_probabilities : int -> int -> tile array array
