3cc17e8b7a4216fd1704ea0ffc6a1e5ebb784d00
Shakeim <shakeim762@gmail.com>
2023-11-17 18:03:01 -0500

    Implemented type for Pokemon sprites and basic functions to store information about their moves & string representations

--- lib/pokemon.ml
+++ lib/pokemon.ml
@@ -1,38 +1,493 @@
-type pokemon = | Charizard 
-| Squirtle
-| Beedrill
-| Ratticate
-| Spearow
-| Pikachu
-| Nidoran
-| Jigglypuff
-| Golbat
-| Parasect
-| Diglett
-| Meowth
-| Poliwhirl
-| Abra
-| Geodude
-| Mewto
-| Haunter
-| Eevee
-| Mewto 
-| Oshawott
+module Pokemon  = struct
+  type pokemon = 
+  | Charizard 
+  | Squirtle
+  | Beedrill
+  | Raticate
+  | Spearow
+  | Pikachu
+  | Nidoran
+  | Jigglypuff
+  | Golbat
+  | Parasect
+  | Diglett
+  | Meowth
+  | Poliwhirl
+  | Abra
+  | Geodude
+  | Mewtwo
+  | Haunter
+  | Eevee
+  | Pyroar
+  | Oshawott
+  
+  type move_tp =
+  | Normal
+  | Electric
+  | Steel
+  | Flying
+  | Water
+  | Ice 
+  | Fighting
+  | Poison
+  | Ghost
+  | Psychic
+  | Ground
+  | Grass 
+  | Fire
+  | Dark
+  
+  type moves = {
+    fighter : pokemon;
+    name : string;
+    tp : int;
+    bp: move_tp;
+    damage : int;
+    descr : string;
+  } 
+  
+  type t = {
 
-type tp = 
-| Electric
-| Water
-| Fire
-| Grass 
-| Ice 
-| Fighting
-| Poison
-| Ground 
-| Flying
-| Psychic
-| Steel
+    (* Pokemon name *)
+    pokemon_name : string;
 
-let moves = {fighter : pokemon; name : string; power : int}
-let move_attributes f n p = {fighter = f; name = n ; power = n}
+    (* Health of the Pokemon *)
+    health : int;
 
-let moves_list = [move_attributes Pikachu "Thunder Shock" 5]
+    (* List of moves a Pokemon can do;
+      specific to a Pokemon *)
+    move_list : moves list;
+
+    (* Type of an attack *)
+    sp: move_tp;
+
+    (* Image of Pokemon in battle *)
+    battle_image : string;
+     
+    (* Image of Pokemon for profile picture *)
+    search_image : string;
+
+    (* Description of Pokemon *)
+    descr : string
+  }
+
+  let name = function
+  | Charizard -> "Charizard"
+  | Squirtle -> "Squirtle"
+  | Beedrill -> "Beedrill"
+  | Raticate -> "Raticate"
+  | Spearow -> "Spearow"
+  | Pikachu -> "Pikachu"
+  | Nidoran -> "Nidoran"
+  | Jigglypuff -> "Jigglypuff"
+  | Golbat -> "Golbat"
+  | Parasect -> "Parasect"
+  | Diglett -> "Diglett"
+  | Meowth -> "Meowth"
+  | Poliwhirl -> "Poliwhirl"
+  | Abra -> "Abra"
+  | Geodude -> "Geodude"
+  | Mewtwo -> "Mewto"
+  | Haunter -> "Haunter"
+  | Eevee -> "Eevee"
+  | Pyroar -> "Pyroar"
+  | Oshawott -> "Oshawott"
+
+  let typ = function
+  | Normal -> "Normal"
+  | Electric -> "Electric"
+  | Steel -> "Steel"
+  | Flying -> "Flying"
+  | Water -> "Water"
+  | Ice -> "Ice"
+  | Fighting -> "Fighting"
+  | Poison -> "Poison"
+  | Ghost -> "Ghost"
+  | Psychic -> "Psychic"
+  | Ground -> "Ground"
+  | Grass -> "Grass"
+  | Fire -> "Fire"
+  | Dark -> "Dark"
+
+  let battle_images = function
+  | Charizard -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/charizard.png"
+  | Squirtle -> 
+  "https://img.pokemondb.net/sprites/black-white/shiny/squirtle.png"
+  | Beedrill -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/beedrill.png"
+  | Raticate -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/raticate-f.png"
+  | Spearow -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/spearow.png"
+  | Pikachu -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/pikachu-f.png"
+  | Nidoran -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/nidoran-f.png"
+  | Jigglypuff -> 
+  "https://img.pokemondb.net/sprites/x-y/normal/jigglypuff.png"
+  | Golbat -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/golbat-f.png"
+  | Parasect -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/parasect.png"
+  | Diglett ->
+  "https://img.pokemondb.net/sprites/black-white/normal/diglett.png"
+  | Meowth -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/meowth.png"
+  | Poliwhirl ->
+  "https://img.pokemondb.net/sprites/black-white/normal/poliwhirl.png"
+  | Abra -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/abra.png"
+  | Geodude ->
+  "https://img.pokemondb.net/sprites/black-white/normal/geodude.png"
+  | Mewtwo -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/mewtwo.png"
+  | Haunter ->
+  "https://img.pokemondb.net/sprites/black-white/normal/haunter.png"
+  | Eevee -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/eevee.png"
+  | Pyroar -> 
+  "https://img.pokemondb.net/sprites/x-y/normal/pyroar-f.png"
+  | Oshawott -> 
+  "https://img.pokemondb.net/sprites/black-white/normal/oshawott.png"
+
+  let search_image = function
+  | Charizard ->
+  "https://img.pokemondb.net/artwork/large/charizard.jpg"
+  | Squirtle ->
+  "https://img.pokemondb.net/artwork/large/squirtle.jpg"
+  | Beedrill ->
+  "https://img.pokemondb.net/artwork/large/beedrill.jpg"
+  | Raticate ->
+  "https://img.pokemondb.net/artwork/large/raticate.jpg"
+  | Spearow ->
+  "https://img.pokemondb.net/artwork/large/spearow.jpg"
+  | Pikachu -> 
+  "https://img.pokemondb.net/artwork/large/pikachu.jpg"
+  | Nidoran ->
+  "https://img.pokemondb.net/artwork/large/nidoran-f.jpg"
+  | Jigglypuff -> 
+  "https://img.pokemondb.net/artwork/large/jigglypuff.jpg"
+  | Golbat ->
+  "https://img.pokemondb.net/artwork/large/golbat.jpg"
+  | Parasect ->
+  "https://img.pokemondb.net/artwork/large/parasect.jpg"
+  | Diglett ->
+  "https://img.pokemondb.net/artwork/large/diglett.jpg"
+  | Meowth -> 
+  "https://img.pokemondb.net/artwork/large/meowth.jpg"
+  | Poliwhirl ->
+  "https://img.pokemondb.net/artwork/large/poliwhirl.jpg"
+  | Abra -> 
+  "https://img.pokemondb.net/artwork/large/abra.jpg"
+  | Geodude -> 
+  "https://img.pokemondb.net/artwork/large/geodude.jpg"
+  | Mewtwo ->
+  "https://img.pokemondb.net/artwork/large/mewtwo.jpg"
+  | Haunter ->
+  "https://img.pokemondb.net/artwork/large/haunter.jpg"
+  | Eevee -> 
+  "https://img.pokemondb.net/artwork/large/eevee.jpg"
+  | Pyroar ->
+  "https://img.pokemondb.net/artwork/large/pyroar.jpg"
+  | Oshawott ->
+  "https://img.pokemondb.net/artwork/large/oshawott.jpg"
+
+  let create_move p n d t da de=
+    {
+      fighter = p;
+      name = n;
+      tp = d;
+      bp = t;
+      damage = da;
+      descr = de
+    }
+
+  let effectivity_list =
+    [
+      (* Normal *)
+      (Normal, 
+      [
+        1.0;
+        1.0;
+        0.5;
+        1.0;
+        1.0;
+        1.0;
+        1.0;
+        1.0;
+        0.0;
+        1.0;
+        1.0;
+        1.0;
+        1.0;
+        1.0;
+    ]
+    );
+
+    (* Electric *)
+    (Electric,
+    [
+      1.0;
+      0.5;
+      1.0;
+      2.0;
+      2.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      0.0;
+      0.5;
+      1.0;
+      1.0
+    ]
+    );
+
+    (* Steel *)
+    (Steel, 
+    [
+      1.0;
+      0.5;
+      0.5;
+      1.0;
+      0.5;
+      2.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      0.5;
+      1.0
+    ]
+    );
+
+    (* Flying *)
+    (Flying, 
+    [
+      1.0;
+      0.5;
+      0.5;
+      1.0;
+      1.0;
+      1.0;
+      2.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      2.0;
+      1.0;
+      1.0
+    ]
+    );
+
+    (* Water *)
+    (Water,
+    [
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      0.5;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      2.0;
+      0.5;
+      2.0;
+      1.0
+    ]
+    );
+
+    (* Ice *)
+    (Ice,
+    [
+      1.0;
+      1.0;
+      0.5;
+      2.0;
+      0.5;
+      0.5;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      2.0;
+      2.0;
+      0.5;
+      1.0
+    ]
+    );
+
+    (* Fighting *)
+    (Fighting, 
+    [
+      2.0;
+      1.0;
+      2.0;
+      0.5;
+      1.0;
+      2.0;
+      1.0;
+      0.5;
+      0.0;
+      0.5;
+      1.0;
+      1.0;
+      1.0;
+      2.0
+    ]
+    );
+
+    (* Poison *)
+    (Poison, 
+    [
+      1.0;
+      1.0;
+      0.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      0.5;
+      0.5;
+      1.0;
+      0.5;
+      2.0;
+      1.0;
+      1.0
+    ]
+    );
+
+    (* Ghost *)
+    (Ghost,
+    [
+      0.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      2.0;
+      2.0;
+      1.0;
+      1.0;
+      1.0;
+      0.5
+    ]
+    );
+
+    (* Psychic *)
+    (Psychic,
+    [
+      1.0;
+      1.0;
+      0.5;
+      1.0;
+      1.0;
+      1.0;
+      2.0;
+      2.0;
+      1.0;
+      0.5;
+      1.0;
+      1.0;
+      1.0;
+      0.0
+    ]
+    );
+
+    (* Ground *)
+    (Ground,
+    [
+      1.0;
+      2.0;
+      2.0;
+      0.0;
+      1.0;
+      1.0;
+      1.0;
+      2.0;
+      1.0;
+      1.0;
+      1.0;
+      0.5;
+      1.0;
+      1.0;
+    ]
+    );
+
+    (* Grass *)
+    (Grass,
+    [
+      1.0;
+      1.0;
+      0.5;
+      0.5;
+      2.0;
+      1.0;
+      1.0;
+      0.5;
+      1.0;
+      1.0;
+      2.0;
+      0.5;
+      0.5;
+      1.0
+    ]
+    );
+
+    (* Fire *)
+    (Fire,
+    [
+      1.0;
+      1.0;
+      2.0;
+      1.0;
+      0.5;
+      2.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      2.0;
+      0.5;
+      1.0
+    ]
+    );
+
+    (* Dark *)
+    (Dark,
+    [
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      1.0;
+      0.5;
+      1.0;
+      2.0;
+      2.0;
+      1.0;
+      1.0;
+      1.0;
+      0.5
+    ]
+    );
+    ]
+
+  
+
+
+end
--- lib/pokemon.mli
+++ lib/pokemon.mli
@@ -1,11 +1,44 @@
 (** The Pokemon type and its attributes to be used and displayed *)
-module type Pokemon_Sprite = sig
+module type PokemonSprite = sig
+
+  (** Represents Pokemon type of a sprite *)
+  type pokemon
+
+  (** Represents species of a Pokemon *)
+  type move_tp
+
+  (** Represents Pokemon move *)
+  type moves
 
   (** Representation type for Pokemon *)
   type t
 
-  (** Returns a list of combat moves for a given Pokemon *)
-  val combat_moves : t -> 'a HashTbl -> string list
+  (** Returns string representation of the Pokemon*)
+  val name : pokemon -> string
 
-  (***)
+  (** Returns string representation of Pokemon type*)
+  val typ : move_tp -> string 
+
+  (** A list of Pokemon images for battle*)
+  val battle_images : pokemon -> string 
+
+  (** A list of Pokemon images for information tab*)
+  val search_images : pokemon -> string 
+
+  (** Creates a Pokemon move with related information*)
+  val create_move : pokemon -> string -> int ->
+    move_tp -> moves
+
+  (** An association list with keys as a attack types,
+    values are lists of attack effecticeness to other 
+    Pokemon species *)
+    val effectivity_list : (move_tp * float list) list 
+
+  (** Calculate the damage of a move on another type of
+    Pokemon*)
+    val calculate_damage : move_tp -> move_tp -> moves -> int
+   
+  (** A list of Pokemon moves and their attributes *)
+  val moves_list : (moves list) list
+
 end
