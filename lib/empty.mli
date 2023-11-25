3ad4435b9a9858b44f61f74c2cc2e231ef9efc1e
Shakeim <shakeim762@gmail.com>
2023-11-25 11:47:20 -0500

    Implemented remaining Tile features: Wall, Health, and Empty

---
+++ lib/empty.mli
@@ -0,0 +1,9 @@
+(** A special feature that a tile can have.
+    Has no affect on the player*)
+    module type Empty = sig
+      (** Representation type for empty*)
+      type t
+
+      (** An instance of empty feature*)
+      val empty : t
+    end
