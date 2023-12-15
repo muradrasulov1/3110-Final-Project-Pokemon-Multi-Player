b2971c5263cb2dc3ee50c32b98c1500be4cdb097
Shakeim <shakeim762@gmail.com>
2023-12-15 14:45:38 -0500

    Included to support testing Experimenting module, used to run game

---
+++ lib/game.ml
@@ -0,0 +1,6 @@
+(** The entry point to start the game. It relies on the Experimenting module to
+    initialize and run the game logic. *)
+
+    let () =
+    (* Call this function to start the game *)
+    Experimenting.start_game ()
